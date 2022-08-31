4883fb6fa700b77d3f02c82f3dab6a411ba6ec2e
BOSTONE069 <bostoneochieng69@gmail.com>
2022-08-31 17:08:03 -0400

    Console commands

--- console.py
+++ console.py
@@ -2,10 +2,15 @@
 
 import cmd
 import sys
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+
 
 class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) ' if sys.__stdin__.isatty() else ''
     file = None
+
+    className = {'BaseModel': BaseModel}
 
     def do_quit(self, arg):
         'Quit command to exit program'
@@ -27,14 +32,30 @@ class HBNBCommand(cmd.Cmd):
         if self.file and 'playback' not in line:
             print(line, file=self.file)
         return line
+
     def close(self):
         if self.file:
             self.file.close()
             self.file = None
 
+    def do_create(self, arg):
+        """Used in creation of class instance """
+        if not arg:
+            print("** class name missing **")
+        elif arg not in HBNBCommand.className.keys():
+            print("** class doesn't exist **")
+        else:
+            obj = HBNBCommand.className[arg]()
+            HBNBCommand.className[arg].save(obj)
+            print(obj.id)
+
+
+
+
 def parse(arg):
     'Convert a series of zero or more numbers to an argument tuple'
     return tuple(map(int, arg.split()))
 
+
 if __name__ == '__main__':
-   HBNBCommand().cmdloop()
+    HBNBCommand().cmdloop()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,6 +2,7 @@
 """This module defines a class to manage file storage for hbnb clone"""
 import json
 
+
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
     __file_path = 'file.json'
@@ -32,6 +33,6 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
